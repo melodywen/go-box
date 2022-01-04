62e5f69a32f73126c46cb733d8ea2cc19c1d5e18
jerry <18643565743@gmail.com>
2022-01-04 14:09:26 +0800

    添加

--- foundation/bootstrap/configure_manager.go
+++ foundation/bootstrap/configure_manager.go
@@ -1,9 +1,87 @@
 package bootstrap
 
-type ConfigureManager struct {
+import (
+	"fmt"
+	"github.com/melodywen/go-box/contracts/foundation"
+	"github.com/spf13/viper"
+	"path"
+	"time"
+)
 
+type ConfigureManager struct {
+	app   foundation.ApplicationInterface
+	viper *viper.Viper
 }
 
-func NewConfigureManager() *ConfigureManager {
-	return &ConfigureManager{}
+func NewConfigureManager(app foundation.ApplicationInterface) *ConfigureManager {
+	manager := &ConfigureManager{
+		app:   app,
+		viper: viper.New(),
+	}
+
+	manager.viper.SetConfigType("yaml")
+	return manager
+}
+
+func (manager *ConfigureManager) WriteConfig() {
+	err := manager.viper.WriteConfigAs(path.Join(manager.app.BootstrapPath(""), "cache", ".config"))
+	if err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error write config file: %w \n", err))
+	}
+}
+
+func (manager *ConfigureManager) Get(key string) interface{} {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetBool(key string) bool {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetFloat64(key string) float64 {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetInt(key string) int {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetIntSlice(key string) []int {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetString(key string) string {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetStringMap(key string) map[string]interface{} {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetStringMapString(key string) map[string]string {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetStringSlice(key string) []string {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetTime(key string) time.Time {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) GetDuration(key string) time.Duration {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) IsSet(key string) bool {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) AllSettings() map[string]interface{} {
+	panic("implement me")
+}
+
+func (manager *ConfigureManager) Set(key string, value interface{}) {
+	manager.viper.Set(key, value)
 }
--- foundation/bootstrap/load_configuration.go
+++ foundation/bootstrap/load_configuration.go
@@ -1,7 +1,11 @@
 package bootstrap
 
 import (
+	"fmt"
 	"github.com/melodywen/go-box/contracts/foundation"
+	"github.com/spf13/viper"
+	"os"
+	"path"
 )
 
 type LoadConfiguration struct {
@@ -9,13 +13,15 @@ type LoadConfiguration struct {
 	configureManager *ConfigureManager
 }
 
+// NewLoadConfiguration  new an config  bootstrap instance
 func NewLoadConfiguration() *LoadConfiguration {
 	return &LoadConfiguration{}
 }
 
+// Bootstrap Register services.
 func (bootstrap *LoadConfiguration) Bootstrap(app foundation.ApplicationInterface) {
 	bootstrap.app = app
-	bootstrap.configureManager = NewConfigureManager()
+	bootstrap.configureManager = NewConfigureManager(app)
 
 	// Next we will spin through all the configuration files in the configuration
 	// directory and load each one into the repository. This will make all of the
@@ -28,11 +34,45 @@ func (bootstrap *LoadConfiguration) Bootstrap(app foundation.ApplicationInterfac
 
 //loadConfigurationFiles load the configuration items from all the files.
 func (bootstrap *LoadConfiguration) loadConfigurationFiles(app foundation.ApplicationInterface, manager *ConfigureManager) {
-	bootstrap.getConfigurationFiles(app)
+	files := bootstrap.getConfigurationFiles(app)
+	configPath := app.ConfigPath("")
+	allSuffix := map[string]bool{
+		"json": true, "toml": true, "yaml": true,
+	}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+		filename := path.Base(file.Name())
+		fileSuffix := path.Ext(filename)
+		if fileSuffix == "" {
+			continue
+		}
+		if _, ok := allSuffix[fileSuffix[1:]]; !ok {
+			continue
+		}
+		filePrefix := filename[0 : len(filename)-len(fileSuffix)]
+
+		config := viper.New()
+		config.SetConfigName(filePrefix)     //name of config file (without extension)
+		config.SetConfigType(fileSuffix[1:]) // REQUIRED if the config file does not have the extension in the name
+		config.AddConfigPath(configPath)     // path to look for the config file in
+		err := config.ReadInConfig()         // Find and read the config file
+		if err != nil {                      // Handle errors reading the config file
+			panic(fmt.Errorf("Fatal error config file: %w \n", err))
+		}
+		manager.Set(filePrefix, config.AllSettings())
+	}
+	manager.WriteConfig()
 }
 
 //getConfigurationFiles Get all the configuration files for the application.
-func (bootstrap *LoadConfiguration) getConfigurationFiles(app foundation.ApplicationInterface) {
-
-
+func (bootstrap *LoadConfiguration) getConfigurationFiles(app foundation.ApplicationInterface) []os.DirEntry {
+	configPath := app.ConfigPath("")
+	dir, err := os.ReadDir(configPath)
+	if err != nil {
+		panic(err)
+	}
+	return dir
 }
